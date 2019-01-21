25610545366f8a2a7be20c127c0be408f52a06f4
Mircea Roata <mircearoatapalade@gmail.com>
2019-01-21 15:53:42 +0200

    Fixed test_no not divisor of 100 and compiler

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import shutil
-import os
-import subprocess
 import argparse
-import psutil
+import os
+import shutil
+import subprocess
+
 # import resource
 
 local_dir = os.path.dirname(os.path.abspath(__file__))
@@ -46,8 +46,8 @@ def evaluate_test(test_no):
     except subprocess.CalledProcessError as err:
         os.remove(str.format("{0}/{1}/exe/{1}.in", local_dir, problem_name))
         return [0, str.format("Error: {0}", err.returncode)]
-    os.remove(str.format("{0}/{1}/exe/{1}.in", local_dir, problem_name))
     if args.verifier == '':
+        os.remove(str.format("{0}/{1}/exe/{1}.in", local_dir, problem_name))
         # noinspection PyBroadException
         try:
             with open(str.format("{0}/{1}/exe/{1}.out", local_dir, problem_name)) as of:
@@ -73,49 +73,66 @@ def evaluate_test(test_no):
             if output[line] != ok[line]:
                 return [0, str.format("Diff: line {0}", line + 1)]
     else:
-        output, err = subprocess.Popen(str.format("{0}\\{1}\\tests\\{2}.exe", local_dir, problem_name, args.verifier),
+        output, err = subprocess.Popen(str.format("{0}\\{1}\\tests\\{2}", local_dir, problem_name, args.verifier),
                                        cwd=str.format("{0}\\{1}\\exe", local_dir, problem_name),
                                        stdout=subprocess.PIPE).communicate()
+        os.remove(str.format("{0}/{1}/exe/{1}.in", local_dir, problem_name))
         if err is not None:
             return [0, str.format("Verifier: {0}", err)]
 
-        output = int(output)
-
         if output == 0:
             return [1, "Verifier: OK!"]
-        elif output == -1:
-            return [0, "Verifier: No Output!"]
         else:
-            return [0, "Verifier: Wrong Answer!"]
+            return [0, str.format("Verifier: {0}", output.decode("utf-8"))]
 
     return [1, "Diff: OK!"]
 
 
 if __name__ == "__main__":
     if file_name.endswith('.cpp'):
-        subprocess.call(str.format('g++ -Wall -O2 -std=c++14 -static {0} -lm -o {1}', file_name,
+        subprocess.call(str.format('g++ -Wall -O2 -std=c++14 -static {0} -lm -o ..\\exe\\{1}', file_name,
                                    file_name[:-4]),
-                        cwd=str.format("{0}\\{1}\\exe", local_dir, problem_name))
+                        cwd=str.format("{0}\\{1}\\source", local_dir, problem_name))
         file_name = str(file_name[:-4] + '.exe')
     elif file_name.endswith('.c'):
-        subprocess.call(str.format('gcc -Wall -O2 -std=c11 -static {0} -lm -o {1}', file_name,
+        subprocess.call(str.format('gcc -Wall -O2 -std=c11 -static {0} -lm -o ..\\exe\\{1}', file_name,
                                    file_name[:-4]),
-                        cwd=str.format("{0}\\{1}\\exe", local_dir, problem_name))
+                        cwd=str.format("{0}\\{1}\\source", local_dir, problem_name))
         file_name = str(file_name[:-4] + '.exe')
     elif file_name.endswith('.exe'):
         pass
     else:
         print("Could not detect best compiler. Specify one with --compiler (gcc, g++)")
     if args.generator != '':
+        import os
+        import glob
+
+        # Get a list of all the file paths that ends with .txt from in specified directory
+        fileList = glob.glob(str.format("{0}\\{1}\\tests\\*.in", local_dir, problem_name))
+        for filePath in fileList:
+            # noinspection PyBroadException
+            try:
+                os.remove(filePath)
+            except Exception:
+                print("Error while deleting file : ", filePath)
+
+        fileList = glob.glob(str.format("{0}\\{1}\\tests\\*.ok", local_dir, problem_name))
+        for filePath in fileList:
+            # noinspection PyBroadException
+            try:
+                os.remove(filePath)
+            except Exception:
+                print("Error while deleting file : ", filePath)
+
         tests_gen = []
         for i in range(args.tests):
-            subprocess.call(str.format("{0}\\{1}\\tests\\{2}.exe", local_dir, problem_name, args.generator),
+            subprocess.call(str.format("{0}\\{1}\\tests\\{2}", local_dir, problem_name, args.generator),
                             cwd=str.format("{0}\\{1}\\tests", local_dir, problem_name))
             shutil.copy(str.format("{0}/{1}/tests/{1}.in", local_dir, problem_name),
                         str.format("{0}/{1}/tests/{2}-{1}.in", local_dir, problem_name, i))
             shutil.copy(str.format("{0}/{1}/tests/{1}.ok", local_dir, problem_name),
                         str.format("{0}/{1}/tests/{2}-{1}.ok", local_dir, problem_name, i))
-            tests_gen.append(str.format('{0} {1}', i, 100//args.tests))
+            tests_gen.append(str.format('{0} {1}\n', i, 100/args.tests))
         with open(str.format('{0}/{1}/tests/tests.txt', local_dir, problem_name), 'w') as f:
             f.writelines(tests_gen)
 
@@ -125,7 +142,7 @@ if __name__ == "__main__":
     for i in range(len(tests)):
         p = tests[i].split(' ')[1]
         res = evaluate_test(tests[i].split(' ')[0])
-        print(str.format("Test: {0} ~ {1} ~ {2}p", tests[i].split(' ')[0], res[1], res[0] * int(p)))
-        total += res[0] * int(p)
+        print(str.format("Test: {0} ~ {1} ~ {2}p", tests[i].split(' ')[0], res[1], res[0] * float(p)))
+        total += res[0] * float(p)
     print(str.format("Total: {0}p", total))
 
